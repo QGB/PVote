5f7e2d3421e2216dcb3371351ea20b70182248e6
QGB <qgbcs1@gmail.com>
2015-12-10 23:35:50 +0800

    demo

--- PVote.py
+++ PVote.py
@@ -1,30 +1,48 @@
+types='''html,htm,png,jpg,js,css,ttf,woff,cur'''.split(',')
+#cur got problem
 from extended_BaseHTTPServer import serve,route,redirect,override
 from qgb import U,T
-
+import os,sys;basedir=os.path.dirname(__file__)
 @route("/",["GET"])
 def index(**kwargs):
-	return str(kwargs)
-
-	if "name" in kwargs:
-		return "Bonjour {0} <br /> <a href='/redirect'>Retour</a>".format(kwargs["name"][0])
-	else:
-		return "Index <br /> <a href='/?name=valentin'>Test Valentin</a><br /> <a href='/form'>Form Test</a>"
-	
+	return redirect('/DEMO.html')
 	
 @route("/r",["GET",'POST'])
 def r(location="http://baidu.com"):
 	return {"content":"","code":301,"Location":location}
 
-@route("/b",["GET",'POST'])
-def b(location="http://baidu.com"):
+@route("/vote",["GET",'POST'])
+def b(id='Error!'):
 	return {
-	"content":"765432",
+	"content":U.read(basedir+'/bg.jpg','rb'),
 	"code":235,
-	"Location":location,
 	"Cache-Control":"no-cache"
-	}
+	}	
+sfr='''
+@route("/{0}",["GET"])
+def {1}():
+	return {{
+	"content":U.read(basedir+'/{0}','rb'),
+	"code":235,
+	"Cache-Control":"no-cache"
+	}}
+'''
+def search(path):
+	for filename in os.listdir(path):
+		fp = os.path.join(path, filename)
+		if os.path.isfile(fp):
+			for i in types:
+				if filename.endswith('.'+i):
+					filename=T.sub(fp,basedir,'')[1:].replace('\\','/')
+					funcname='fr_'+T.varname(filename)
+					exec(sfr.format(filename,funcname))
+					print filename,funcname
+					break
+		elif os.path.isdir(fp):
+			search(fp)
+search(basedir)
 
-
-ip="0.0.0.0";port=804
+# U.x()
+ip="0.0.0.0";port=80
 U.pln(ip,port)
 serve(ip,port)
